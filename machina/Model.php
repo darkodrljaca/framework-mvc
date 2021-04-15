81f6993d69fd06420a9f0189e0d3012cbf00f366
lookaround3 <darkodrljaca@gmail.com>
2021-04-15 08:29:35 +0200

    Display labels in error messages

--- machina/Model.php
+++ machina/Model.php
@@ -34,6 +34,16 @@ abstract class Model {
     
     abstract public function rules(): array; 
     
+    public function labels(): array {
+        
+        return [];
+        
+    }
+    
+    public function getLabel($attribute) {
+        return $this->labels()[$attribute] ?? $attribute;
+    }
+    
     public array $errors = [];
     
     public function validate() {
@@ -66,6 +76,7 @@ abstract class Model {
                     $this->addError($attribute, self::RULE_MAX, $rule);
                 }
                 if($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
+                    $rule['match'] = $this->getLabel($rule['match']);
                     $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
                 if($ruleName === self::RULE_UNIQUE) {
@@ -77,7 +88,7 @@ abstract class Model {
                     $statement->execute();
                     $record = $statement->fetchObject();
                     if($record) {
-                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $attribute]);
+                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $this->getLabel($attribute)]);
                     }
                 }
                 
